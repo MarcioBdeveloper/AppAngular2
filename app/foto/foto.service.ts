b17765ea1b0736299551d9ca413d40b63a9edf2c
Marcio Barbosa <marciobarbosamobile@gmail.com>
2016-11-04 18:02:28 -0300

    ajuste de url no servico

--- app/foto/foto.service.ts
+++ app/foto/foto.service.ts
@@ -1,7 +1,7 @@
 import { Http, Headers, Response } from '@angular/http';
 import { FotoComponent } from './foto.component';
 import 'rxjs/Rx';
-import {Observable} from 'rxjs'; 
+import { Observable } from 'rxjs'; 
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -9,8 +9,7 @@ export class FotoService {
 
     http: Http;
     headers: Headers;
-    urlLista: string = 'http://localhost:8080/usuario/fotos';
-    urlSalvar: string = 'http://localhost:8080/usuario/fotos/salvar';
+    url: string = 'http://localhost:8080/usuario/fotos';
 
     constructor(http: Http) { 
 
@@ -21,12 +20,12 @@ export class FotoService {
 
     lista(): Observable<FotoComponent[]> {
 
-        return this.http.get(this.urlLista).map(res => res.json());
+        return this.http.get(this.url).map(res => res.json());
     }
 
     cadastra(foto: FotoComponent): Observable<Response> {
 
-        return this.http.post(this.urlSalvar, JSON.stringify(foto), { headers: this.headers }); 
+        return this.http.post(this.url+"/salvar", JSON.stringify(foto), { headers: this.headers }); 
 
     }
 }
