2febc51f42a358402e3806419e63ed33fb1038b9
Marcio Barbosa <marciobarbosamobile@gmail.com>
2016-11-07 18:07:06 -0300

    ajuste no editar e cadastrar

--- app/cadastro/cadastro.component.ts
+++ app/cadastro/cadastro.component.ts
@@ -3,7 +3,7 @@ import { FotoComponent } from '../foto/foto.component';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FotoService } from '../foto/foto.service';
 import { Http } from '@angular/http';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -16,9 +16,11 @@ export class CadastroComponent {
     service: FotoService;
     meuForm: FormGroup;
     route: ActivatedRoute;
+    router: Router;
     mensagem: string = '';
 
-    constructor(service: FotoService, fb: FormBuilder, route: ActivatedRoute){
+    constructor(service: FotoService, fb: FormBuilder, route: ActivatedRoute, router: Router){
+        this.router = router;
         this.route = route;
         this.service = service;
         this.meuForm = fb.group({
@@ -45,9 +47,12 @@ export class CadastroComponent {
 
         this.service.cadastra(this.foto)
             .subscribe(() => {
-                this.foto = new FotoComponent();
                 console.log('Foto salva com sucesso');
                 this.mensagem = 'Foto salva com sucesso';
+                if(this.foto.id){
+                    this.router.navigate(['']);
+                }
+                this.foto = new FotoComponent();
             }, erro => {
                 console.log(erro);
                 this.mensagem = 'Não foi possível salvar a foto';
